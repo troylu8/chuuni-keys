03e86a383b17b33975eb6c8d71783086f4b08bcb
troy <troylu8@gmail.com>
2025-07-30 10:06:23 -0700

    prepare deb build

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,15 +1,18 @@
 use tauri::Manager;
+
+#[cfg(target_os = "windows")]
 use tauri_plugin_prevent_default::PlatformOptions;
 
 mod charts;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-    tauri::Builder::default()
+    let app = tauri::Builder::default()
         .plugin(tauri_plugin_single_instance::init(|app, _, _| {
-            let _ = app.get_webview_window("main")
-                        .expect("should be a main window open")
-                        .set_focus();
+            let _ = app
+                .get_webview_window("main")
+                .expect("should be a main window open")
+                .set_focus();
         }))
         .setup(|app| {
             #[cfg(dev)]
@@ -23,21 +26,20 @@ pub fn run() {
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_process::init())
         .plugin(tauri_plugin_fs::init())
-        .plugin(tauri_plugin_opener::init())
-        .plugin(
+        .plugin(tauri_plugin_opener::init());
+
+    #[cfg(target_os = "windows")]
+    {
+        let app = app.plugin(
             tauri_plugin_prevent_default::Builder::new()
                 .platform(PlatformOptions {
                     general_autofill: false,
                     password_autosave: false,
                 })
                 .build(),
-        )
-        .invoke_handler(tauri::generate_handler![
-            charts::get_all_charts,
-            charts::zip_chart,
-            charts::unzip_chart,
-        ])
-        .run(tauri::generate_context!())
+        );
+    }
+    app.run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,9 @@ import ReactDOM from "react-dom/client";
 import App from "./app";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
 );
 
 // after app is rendered, remove the loading spinner in the corner
